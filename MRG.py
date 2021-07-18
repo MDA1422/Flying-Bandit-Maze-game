029e67e2e6e4b604311ed981ac46cb9bd3af0400
Melik-A1422 <71685940+Melik-A1422@users.noreply.github.com>
2021-07-18 16:00:24 +0100

    adding the first screen
    includes instructions on how to play anf the game objectives

--- MRG.py
+++ MRG.py
@@ -3,25 +3,19 @@ from pygame.locals import *
 
 pygame.init()
 
-screen_width = 1000
-screen_height = 600
+screen_width1 = 768
+screen_height1 = 548
 
-screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption('MRG')
+screen1 = pygame.display.set_mode((screen_width1, screen_height1))
+pygame.display.set_caption('MRG.intro')
 
-
-   
-        
-        
-
-  
-
+game_intro=pygame.image.load('images/gameintro.jpg')
 run = True
 while run:
-
+    screen1.blit(game_intro,(1,1))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-
+    pygame.display.update()
 pygame.quit()
          
