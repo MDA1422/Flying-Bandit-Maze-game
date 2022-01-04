49cf8aba37f35dcd8ce6c5e8480796205228fdc4
MDA1422 <71685940+Melik-A1422@users.noreply.github.com>
2022-01-04 11:47:45 +0000

    update

---
+++ test_right_answer.py
@@ -0,0 +1,12 @@
+import unittest
+import FBM
+
+
+class TestFunc(unittest.TestCase):
+    def test_text(self):
+        self.assertTrue(FBM.right_answer,1)
+        
+        
+
+if __name__ == '__main__':
+    unittest.main()
